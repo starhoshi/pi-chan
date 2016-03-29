f8716a351d0ee69f4f5c45c0c4bcc6ee5d80b690
star__hoshi <kensuke1751@gmail.com>
2016-03-30 02:14:00 +0900

    setting token, team.

---
+++ pi-chan/Apis/Esa.swift
@@ -0,0 +1,21 @@
+//
+//  esa.swift
+//  pi-chan
+//
+//  Created by Kensuke Hoshikawa on 2016/03/27.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+
+public struct Esa{
+  public var token:String!
+  public var currentTeam:String!
+  
+  public init(token: String, currentTeam: String) {
+    self.token = token
+    self.currentTeam = currentTeam
+  }
+
+  
+}
