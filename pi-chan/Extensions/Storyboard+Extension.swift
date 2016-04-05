0972b2f5ab8c98dab2b92b394f8089fc0dccc6b6
star__hoshi <kensuke1751@gmail.com>
2016-04-06 02:59:05 +0900

    add storyboard cell.

--- pi-chan/Extensions/Storyboard+Extension.swift
+++ pi-chan/Extensions/Storyboard+Extension.swift
@@ -9,49 +9,32 @@
 import UIKit
 
 @IBDesignable class CustomButton: UIButton {
-  
-  // 角丸の半径(0で四角形)
   @IBInspectable var cornerRadius: CGFloat = 0.0
-  
-  // 枠
   @IBInspectable var borderColor: UIColor = UIColor.clearColor()
   @IBInspectable var borderWidth: CGFloat = 0.0
   
   override func drawRect(rect: CGRect) {
-    // 角丸
     self.layer.cornerRadius = cornerRadius
     self.clipsToBounds = (cornerRadius > 0)
-    
-    // 枠線
     self.layer.borderColor = borderColor.CGColor
     self.layer.borderWidth = borderWidth
-    
     super.drawRect(rect)
   }
 }
 
 @IBDesignable class CustomLabel: UILabel {
-  
-  // 角丸の半径(0で四角形)
   @IBInspectable var cornerRadius: CGFloat = 0.0
-  
-  // 枠
   @IBInspectable var borderColor: UIColor = UIColor.clearColor()
   @IBInspectable var borderWidth: CGFloat = 0.0
   
   override func drawRect(rect: CGRect) {
-    // 角丸
     self.layer.cornerRadius = cornerRadius
     self.clipsToBounds = (cornerRadius > 0)
-    
-    // 枠線
     self.layer.borderColor = borderColor.CGColor
     self.layer.borderWidth = borderWidth
-    
     super.drawRect(rect)
   }
 }
-
 
 private var flatAssociatedObjectKey: UInt8 = 0
 @IBDesignable extension UINavigationBar {
