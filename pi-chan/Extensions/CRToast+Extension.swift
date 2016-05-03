e2a07a18b9d1d46d395a093e53bfb6d718ec2cfa
star__hoshi <kensuke1751@gmail.com>
2016-05-04 02:47:19 +0900

    change params.

--- pi-chan/Extensions/CRToast+Extension.swift
+++ pi-chan/Extensions/CRToast+Extension.swift
@@ -12,11 +12,12 @@ extension CRToast {
   static func showSuccessNotificationFromBottom(){
     let options = [
       kCRToastTextKey : "Me lleva la mierda",
-      kCRToastAnimationInDirectionKey : CRToastAnimationDirection.Bottom.rawValue,
+      kCRToastAnimationInDirectionKey : CRToastAnimationDirection.Top.rawValue,
       kCRToastAnimationInTypeKey : CRToastAnimationType.Gravity.rawValue,
-      kCRToastAnimationOutDirectionKey : CRToastAnimationDirection.Bottom.rawValue,
+      kCRToastAnimationOutDirectionKey : CRToastAnimationDirection.Top.rawValue,
       kCRToastAnimationOutTypeKey : CRToastAnimationType.Gravity.rawValue,
       kCRToastNotificationPresentationTypeKey : CRToastPresentationType.Push.hashValue,
+      kCRToastNotificationTypeKey : CRToastType.NavigationBar.rawValue,
       kCRToastTextAlignmentKey : NSTextAlignment.Left.rawValue,
       kCRToastTimeIntervalKey : 1.0,
       kCRToastUnderStatusBarKey : NSNumber(bool: false),
